50152c41f6aab8dbef1a83250648b495137c2862
Igiraneza K. Winny <karangwawin8@gmail.com>
2024-12-04 09:47:24 +0200

    created a component folder with relevant files

---
+++ components/MobileResponsiveness/MobileResponsiveness.jsx
@@ -0,0 +1,9 @@
+import  React from 'react';
+import "./mobileresponsiveness.css";
+
+const MobileResponsiveness = () => {
+    return (
+        <div>Mobile Responsiveness</div>
+    )
+}
+export default MobileResponsiveness
---
+++ components/SeoandAnalytics/SeoandAnalytics.jsx
@@ -0,0 +1,9 @@
+import  React from 'react';
+import "./seoandanalytics.css";
+
+const SeoandAnalytics = () => {
+    return (
+        <div>Seo and Analytics</div>
+    )
+}
+export default SeoandAnalytics
